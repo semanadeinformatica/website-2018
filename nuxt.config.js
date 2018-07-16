f8b0ab43e1b6d9c50998e339ce4518743b48f722
António Almeida <theantonioalmeida@gmail.com>
2018-07-16 03:07:06 +0100

    Fix bugs on favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,16 +10,16 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'A Semana de Informática é um evento que decorre nos dias 29 de outubro a 1 de novembro, na Faculdade de Engenharia da Universidade do Porto, onde alunos vão poder passar 4 dias a aprender com especialistas nas diversas áreas de informática, familiarizar-se com o mundo empresarial e conviverem em competições e eventos sociais.' }
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes="180x180" href: '/apple-touch-icon.png' }
-      { rel: 'icon', type: 'image/png', sizes="180x180", href: '/apple-touch-icon.png' }
-      { rel: 'icon', type: 'image/png', sizes="32x32", href: '/favicon-32x32.png' }
-      { rel: 'icon', type: 'image/png', sizes="16x16", href: '/favicon-16x16.png' }
-      { rel: 'manifest', href: '/site.webmanifest' }
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color="#da291c" }
-      { rel: 'shortcut icon', href: '/favicon.ico' }
-      { rel: 'msapplication-TileColor', content="#b91d47" }
-      { rel: 'msapplication-config', content="/browserconfig.xml" }
-      { rel: 'theme-color', content="#000000" }
+      { rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: "180x180", href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: "#da291c" },
+      { rel: 'shortcut icon', href: '/favicon.ico' },
+      { rel: 'msapplication-TileColor', content: "#b91d47" },
+      { rel: 'msapplication-config', content: "/browserconfig.xml" },
+      { rel: 'theme-color', content: "#000000" }
     ]
   },
 
