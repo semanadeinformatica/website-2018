bef0d87c7f081d8d043aca9b0e2dfdb02cfb4b92
António Almeida <theantonioalmeida@gmail.com>
2018-07-16 01:53:06 +0100

    First commit with base project and 'coming soon' page structure'

---
+++ pages/landing.vue
@@ -0,0 +1,33 @@
+<template>
+	<section class="h-100 container">
+			<div class="h-100 d-flex flex-column flex-lg-row justify-content-around align-items-center">
+				<div> <h1> Ola </h1></div>
+
+				<div class="text-center">
+					<h1>semana de informática</h1>
+					<br>
+					<h2>29 outubro - 1 novembro 2018 <br>
+					faculdade de engenharia da universidade do porto</h2>
+				</div>
+
+				<div> <h1> Ola </h1></div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+
+  name: 'Landing',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
