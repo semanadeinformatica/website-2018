26eb4345d43a453fb8a4c3fd7e50116d409e4701
António Almeida <theantonioalmeida@gmail.com>
2018-07-16 03:03:28 +0100

    Add favicons and manifests

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,23 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'bootstrap-website',
+    title: 'semana de informática 2018',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'apple-touch-icon', sizes="180x180" href: '/apple-touch-icon.png' }
+      { rel: 'icon', type: 'image/png', sizes="180x180", href: '/apple-touch-icon.png' }
+      { rel: 'icon', type: 'image/png', sizes="32x32", href: '/favicon-32x32.png' }
+      { rel: 'icon', type: 'image/png', sizes="16x16", href: '/favicon-16x16.png' }
+      { rel: 'manifest', href: '/site.webmanifest' }
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color="#da291c" }
+      { rel: 'shortcut icon', href: '/favicon.ico' }
+      { rel: 'msapplication-TileColor', content="#b91d47" }
+      { rel: 'msapplication-config', content="/browserconfig.xml" }
+      { rel: 'theme-color', content="#000000" }
     ]
   },
 
