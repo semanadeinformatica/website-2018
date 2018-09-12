d765068669f62b93b2719546935bad6fd2708d09
António Almeida <theantonioalmeida@gmail.com>
2018-09-12 01:49:50 +0100

    Restructure nuxtent config according to data && add sample file w/ usage

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -1,14 +1,21 @@
 module.exports = {
 	// content/HelloWorld.md --> speakers/hello-world
-	content: {
-		page: '/speakers/_slug',
-		permalink: ':slug',
+	content: [
+	["talks", {
+		page: '/talks/_talk',
+		permalink: ':year/:slug',
 		isPost: false,
-		generate: [
-		'get',
-		'getAll'
+		generate: [ 
+		'get', 
+		'getAll' 
 		]
-	},
+	}],
+	["workshops", {
+		page: '/workshops/_workshop',
+		permalink: ":slug",
+		isPost: false
+	}]
+	],
 	api: {
 		baseURL: 'http://localhost:8080',
 		browserBaseURL: 'https://sinf.pt'
