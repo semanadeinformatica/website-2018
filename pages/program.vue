66dd7d389cd37922dcf2e59b8268f711f5b324c7
António Almeida <theantonioalmeida@gmail.com>
2018-09-25 14:35:01 +0100

    Add sample day selection tab

--- pages/program.vue
+++ pages/program.vue
@@ -4,112 +4,363 @@
 		<div class="section-header-divider"></div>
 
 		<div class="container mt-5">
-			<div class="row">
-				<div class="offset-lg-2 col-lg-8">
-					<table class="table table-borderless text-lowercase">
-						<thead>
-							<tr>
-								<th scope="col">time</th>
-								<th scope="col"></th>
-								<th scope="col"></th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th scope="row">14h00</th>
-								<td rowspan="2">
-									<div class="event">
-										<div>
+			<nav>
+				<div class="nav nav-tabs text-lowercase" id="nav-tab" role="tablist">
+					<a class="nav-item nav-link active" id="nav-39-oct-tab" data-toggle="tab" href="#nav-29-oct" role="tab" aria-controls="nav-29-oct" aria-selected="true">29 Oct</a>
+					<a class="nav-item nav-link" id="nav-30-oct-tab" data-toggle="tab" href="#nav-30-oct" role="tab" aria-controls="nav-30-oct" aria-selected="false">30 Oct</a>
+					<a class="nav-item nav-link" id="nav-31-oct-tab" data-toggle="tab" href="#nav-31-oct" role="tab" aria-controls="nav-31-oct" aria-selected="false">31 Oct</a>
+					<a class="nav-item nav-link" id="nav-1-nov-tab" data-toggle="tab" href="#nav-1-nov" role="tab" aria-controls="nav-1-nov" aria-selected="false">1 Nov</a>
+				</div>
+			</nav>
+			<div class="tab-content" id="nav-tabContent">
+				<div class="tab-pane fade show active" id="nav-29-oct" role="tabpanel" aria-labelledby="nav-39-oct-tab">
+					<div class="row">
+						<div class="offset-lg-2 col-lg-8">
+							<table class="table table-borderless text-lowercase">
+								<thead>
+									<tr>
+										<th scope="col">time</th>
+										<th scope="col"></th>
+										<th scope="col"></th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">14h00</th>
+										<td class="event" rowspan="2">
 											<h4>Opening Cerimony</h4>
 											<span>what</span><br>
 											<span>what</span>
-										</div>
-									</div>
-								</td>
-								<td rowspan="2">
-									<div class="event">
-										<div>
+										</td>
+										<td class="event" rowspan="3">
 											<h4>Opening Cerimony</h4>
 											<span>what</span><br>
 											<span>what</span>
-										</div>
-									</div>
-								</td>
-							</tr>
-								<tr>
-								<th scope="row">14h30</th>
-							</tr>
-							<tr>
-								<th scope="row">15h00</th>
-								<td></td>
-								<td rowspan="2">
-									<div class="talk">
-										<div>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">14h30</th>
+									</tr>
+									<tr>
+										<th scope="row">15h00</th>
+										<td class="talk" rowspan="2">
 											<h4>talk de cenas</h4>
 											<span>what</span><br>
 											<span>what</span>
-										</div>
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<th scope="row">15h30</th>
-							</tr>
-							<tr>
-								<th scope="row">16h00</th>
-								<td></td>
-							</tr>
-							<tr>
-								<th scope="row">16h30</th>
-								<td></td>
-							</tr>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">15h30</th>
+									</tr>
+									<tr>
+										<th scope="row">16h00</th>
+									</tr>
+									<tr>
+										<th scope="row">16h30</th>
+										<td></td>
+									</tr>
 
-							<tr>
-								<th scope="row">17h00</th>
-								<td rowspan="2">
-									<div class="talk">
-										<div>
+									<tr>
+										<th scope="row">17h00</th>
+										<td class="talk" rowspan="2">
 											<h4>workshop de cenas</h4>
 											<span>what</span><br>
 											<span>what</span>
-										</div>
-									</div>
-								</td>
-								<td></td>
-							</tr>
+										</td>
+										<td></td>
+									</tr>
 
-							<tr>
-								<th scope="row">17h30</th>
-								<td></td>
-							</tr>
-							<tr>
-								<th scope="row">18h00</th>
-								<td rowspan="2">
-									<div class="event">
-										<div>
+									<tr>
+										<th scope="row">17h30</th>
+										<td></td>
+									</tr>
+									<tr>
+										<th scope="row">18h00</th>
+										<td class="event" rowspan="2">
 											<h4>workshop de cenas</h4>
 											<span>what</span><br>
 											<span>what</span>
-										</div>
-									</div>
-								</td>
-								<td></td>
-							</tr>
+										</td>
+										<td></td>
+									</tr>
 
-							<tr>
-								<th scope="row">18h30</th>
-								<td></td>
-							</tr>
+									<tr>
+										<th scope="row">18h30</th>
+										<td></td>
+									</tr>
 
-							<tr>
-								<th scope="row">19h00</th>
-							</tr>
+									<tr>
+										<th scope="row">19h00</th>
+									</tr>
 
-						</tbody>
-					</table>
+								</tbody>
+							</table>
 
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="nav-30-oct" role="tabpanel" aria-labelledby="nav-30-oct-tab">
+					<div class="row">
+						<div class="offset-lg-2 col-lg-8">
+							<table class="table table-borderless text-lowercase">
+								<thead>
+									<tr>
+										<th scope="col">time</th>
+										<th scope="col"></th>
+										<th scope="col"></th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">14h00</th>
+										<td class="event" rowspan="2">
+											<h4>Opening Cerimony</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td class="event" rowspan="3">
+											<h4>Opening Cerimony</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">14h30</th>
+									</tr>
+									<tr>
+										<th scope="row">15h00</th>
+										<td class="talk" rowspan="2">
+											<h4>talk de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">15h30</th>
+									</tr>
+									<tr>
+										<th scope="row">16h00</th>
+									</tr>
+									<tr>
+										<th scope="row">16h30</th>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">17h00</th>
+										<td class="talk" rowspan="2">
+											<h4>workshop de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">17h30</th>
+										<td></td>
+									</tr>
+									<tr>
+										<th scope="row">18h00</th>
+										<td class="event" rowspan="2">
+											<h4>workshop de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">18h30</th>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">19h00</th>
+									</tr>
+
+								</tbody>
+							</table>
+
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="nav-31-oct" role="tabpanel" aria-labelledby="nav-31-oct-tab">
+					<div class="row">
+						<div class="offset-lg-2 col-lg-8">
+							<table class="table table-borderless text-lowercase">
+								<thead>
+									<tr>
+										<th scope="col">time</th>
+										<th scope="col"></th>
+										<th scope="col"></th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">14h00</th>
+										<td class="event" rowspan="2">
+											<h4>Opening Cerimony</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td class="event" rowspan="3">
+											<h4>Opening Cerimony</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">14h30</th>
+									</tr>
+									<tr>
+										<th scope="row">15h00</th>
+										<td class="talk" rowspan="2">
+											<h4>talk de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">15h30</th>
+									</tr>
+									<tr>
+										<th scope="row">16h00</th>
+									</tr>
+									<tr>
+										<th scope="row">16h30</th>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">17h00</th>
+										<td class="talk" rowspan="2">
+											<h4>workshop de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">17h30</th>
+										<td></td>
+									</tr>
+									<tr>
+										<th scope="row">18h00</th>
+										<td class="event" rowspan="2">
+											<h4>workshop de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">18h30</th>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">19h00</th>
+									</tr>
+
+								</tbody>
+							</table>
+
+						</div>
+					</div>
+				</div>
+
+				<div class="tab-pane fade" id="nav-1-nov" role="tabpanel" aria-labelledby="nav-1-nov-tab">
+					<div class="row">
+						<div class="offset-lg-2 col-lg-8">
+							<table class="table table-borderless text-lowercase">
+								<thead>
+									<tr>
+										<th scope="col">time</th>
+										<th scope="col"></th>
+										<th scope="col"></th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">14h00</th>
+										<td class="event" rowspan="2">
+											<h4>Opening Cerimony</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td class="event" rowspan="3">
+											<h4>Opening Cerimony</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">14h30</th>
+									</tr>
+									<tr>
+										<th scope="row">15h00</th>
+										<td class="talk" rowspan="2">
+											<h4>talk de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+									</tr>
+									<tr>
+										<th scope="row">15h30</th>
+									</tr>
+									<tr>
+										<th scope="row">16h00</th>
+									</tr>
+									<tr>
+										<th scope="row">16h30</th>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">17h00</th>
+										<td class="talk" rowspan="2">
+											<h4>workshop de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">17h30</th>
+										<td></td>
+									</tr>
+									<tr>
+										<th scope="row">18h00</th>
+										<td class="event" rowspan="2">
+											<h4>workshop de cenas</h4>
+											<span>what</span><br>
+											<span>what</span>
+										</td>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">18h30</th>
+										<td></td>
+									</tr>
+
+									<tr>
+										<th scope="row">19h00</th>
+									</tr>
+
+								</tbody>
+							</table>
+
+						</div>
+					</div>
 				</div>
 			</div>
+
+
 		</div>
 
 	</section>
@@ -145,8 +396,6 @@
 
 	<style lang="scss" scoped>
 	.table {
-		tr, td, th {
-		}
 		font-size: 1.25rem;
 
 		tr th:first-child {
@@ -164,18 +413,17 @@
 		.talk {
 			background-color: white;
 			color: black;
-			margin: -0.25em;
-			div {
-				padding: 0.5em;
-			}
 		}
 
 		.event {
 			background-color: #DA291C;
-			margin: -0.25em;
-			div {
-				padding: 0.5em;
-			}
+		}
+
+		.event, .talk {
+			background-clip: padding-box;
+			border-style: solid;
+			border-color: transparent;
+			border-width: 0.5em;
 		}
 	}
 
