c61426a29af91693bb1f3486da47ff21a1540659
António Almeida <theantonioalmeida@gmail.com>
2018-09-30 18:07:37 +0100

    Implement necessary changes to talk page to feature launching content

--- pages/talks/_name.vue
+++ pages/talks/_name.vue
@@ -1,60 +1,74 @@
 <template>
   <section class="mt-5">
-    <h2 class="container text-lowercase">speaker</h2>
+    <h2 class="container text-lowercase">talk</h2>
     <div class="section-header-divider"></div>
 
     <div class="container">
 
       <div class="row">
-        <div class="col-lg-3 my-4 pr-4">
-            <img src="https://via.placeholder.com/900x900" class="img-fluid">
+        <div class="col-lg-3 my-5 pr-4 d-flex flex-column">
+          <img :src='"~/assets/img/speakers/" + talk.img + ".jpg"' class="img-fluid">
+          <div class="ml-auto mt-2">
+            <a class="ml-2" v-if='talk.website' :href='talk.website'><i class="fas fa-external-link-square-alt fa-2x"></i></a>
+            <a class="ml-2" v-if='talk.linkedin' :href='talk.linkedin'><i class="fab fa-linkedin fa-2x"></i></a>
+            <a class="ml-2" v-if='talk.twitter' :href='talk.twitter'><i class="fab fa-twitter-square fa-2x"></i></a>
+          </div>
         </div>
-        <div class="col-lg-9 p-4">
+        <div class="col-lg-9 mt-3 p-4">
           <div class="text-lowercase">
-              <h1> {{ talk.speaker }} </h1>
-              <h4 class="body-style"> {{ talk.occupation }} </h4>
-              <h4 class="body-style"> {{ talk.workplace }} </h4>
+            <h1> {{ talk.speaker }} </h1>
+            <h4 class="body-style"> {{ talk.occupation }} </h4>
+            <h4 class="body-style"> {{ talk.workplace }} </h4>
           </div>
 
-          <p class="lead mt-4">{{ talk.bio }}</p>
+          <p class="lead mt-4">
+            <span v-if='talk.name1' class="text-strong">{{ talk.name1 }}<br></span>
+            {{ talk.bio }}
+          </p>
+          <p v-if='talk.bio2' class="lead mt-4">
+           <span class="text-strong">{{ talk.name2 }}</span><br>
+         {{ talk.bio2 }}</p>
 
-          <div class="text-lowercase">
-              <h1> {{ talk.title }} </h1>
-              <h4 class="body-style"> {{ talk.day }}, {{ talk.time }}, room {{ talk.place }}. </h4>
-          </div>
+         <div class="text-lowercase">
+          <h1> {{ talk.title }} </h1>
+          <h4 class="body-style"> {{ talk.day }}, {{ talk.time }}, room {{ talk.place }}. </h4>
+        </div>
 
-          <nuxtent-body class="mt-4 lead" :body="talk.body" />
+        <nuxtent-body class="mt-4 lead" :body="talk.body" />
       </div>
-  </div>
+    </div>
 
-</div>
+  </div>
 </section>
 </template>
 
 <script>
-    export default {
-    
+  export default {
+
     head () {
-        return {
-            title: this.talk.title + " | SINF",
-        }
+      return {
+        title: this.talk.title + " | semana de informatica",
+      }
     },
 
-     async asyncData({ app, route }) {
+    async asyncData({ app, route }) {
       return {
         talk: await app.$content("/").get(route.path)
+      }
+    },
+
+    name: 'Talk',
+
+    data () {
+      return {
+
+      }
     }
-},
-
-name: 'Talk',
-
-data () {
-    return {
-
-    }
-}
-}
+  }
 </script>
 
 <style lang="css" scoped>
+.text-strong {
+  font-weight: 600;
+}
 </style>
